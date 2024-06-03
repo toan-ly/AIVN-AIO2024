b8adbf0d5f83754dba0aab2d40ce10755d6549c4
toan-ly <toanly0521@gmail.com>
2024-06-03 01:53:37 -0400

    add feature/module1-week1-basic-python

---
+++ module1/week1-BasicPython/is_number.py
@@ -0,0 +1,11 @@
+def is_number(n):
+    try:
+        float(n)
+        return True
+    except ValueError:
+        return False
+    
+assert is_number(3) == 1.0
+assert is_number('-2a') == 0.0
+print(is_number(1))
+print(is_number('n'))
---
+++ module1/week1-BasicPython/losses.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+def calc_ae(y, y_hat):
+    if not isinstance(y, (int, float)) or not isinstance(y_hat, (int, float)):
+        raise ValueError('Input must be numeric')
+    return np.abs(y - y_hat)
+
+def calc_se(y, y_hat):
+    if not isinstance(y, (int, float)) or not isinstance(y_hat, (int, float)):
+        raise ValueError('Input must be numeric')
+    return (y - y_hat) ** 2
+
+def md_nre_single_sample(y, y_hat, n, p):
+    y_root = y ** (1/n)
+    y_hat_root = y_hat ** (1/n)
+    difference = y_root - y_hat_root
+    loss = difference ** p
+    return loss
+
+assert calc_ae(1, 6) == 5
+print(calc_ae(2, 9))
+
+assert calc_se(4, 2) == 4
+print(calc_se(2, 1))
+
+print(md_nre_single_sample(y=100, y_hat=99.5, n=2, p=1))
+print(md_nre_single_sample(y=50, y_hat=49.5, n=2, p=1))
+print(md_nre_single_sample(y=20, y_hat=19.5, n=2, p=1))
+print(md_nre_single_sample(y=0.6, y_hat=0.1, n=2, p=1))
